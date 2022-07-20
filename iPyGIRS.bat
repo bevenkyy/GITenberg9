48f247ac05a123ddd3bd58d07079bdeb85cefa63
bevenkyy <fengshichank700@163.com>
2022-07-20 15:25:01 +0800

    重新添加了“iPyGIRS.bat”文件，允许用户修改其内容从而使用系统中已安装的Python解释器

---
+++ iPyGIRS.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+SET batFileDir=%cd%
+SET pythonExeFilePath=/python-3.7.5-embed-amd64/python.exe
+SET MainAppFilePath=/core/main.py
+SET pythonExeAbsFilePath=%batFileDir%%pythonExeFilePath%
+REM SET pythonExeAbsFilePath=Python
+SET MainAppAbsFilePath=%batFileDir%%MainAppFilePath%
+SET "spaceChar= "
+SET RunCommand=%pythonExeAbsFilePath%%spaceChar%%MainAppAbsFilePath%
+
+ECHO.
+
+ECHO %RunCommand%
+
+ECHO iPyGIRS(Version V0.2.1) application is starting, please wait a moment...
+
+ECHO.
+
+%RunCommand%
+
+PAUSE

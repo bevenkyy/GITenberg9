e6083caead999b14fde205c2057f2231fb034624
bevenkyy <fengshichank700@163.com>
2022-12-03 12:24:53 +0800

    更新了启动脚本，python解释器所在目录现在改为python，更为简洁

--- iPyGIRS.bat
+++ iPyGIRS.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET batFileDir=%cd%
-SET pythonExeFilePath=/python-3.7.5-embed-amd64/python.exe
+SET pythonExeFilePath=/python/python.exe
 SET MainAppFilePath=/src/main.py
 SET pythonExeAbsFilePath=%batFileDir%%pythonExeFilePath%
 REM SET pythonExeAbsFilePath=Python
@@ -11,7 +11,7 @@ SET RunCommand=%pythonExeAbsFilePath%%spaceChar%%MainAppAbsFilePath%
 
 ECHO.
 
-ECHO iPyGIRS(Version 0.2.3) application is starting, please wait a moment...
+ECHO iPyGIRS(Version 0.2.4) application is starting, please wait a moment...
 
 ECHO.
 
